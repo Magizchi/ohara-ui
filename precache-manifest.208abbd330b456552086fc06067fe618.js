b1b6f8b64d599ad75d744cb29ab27a62bf8eebb1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-24 19:35:19 +0000

    Deploy Magizchi/ohara-ui to github.com/Magizchi/ohara-ui.git:gh-pages

---
+++ precache-manifest.208abbd330b456552086fc06067fe618.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76c42aba0c2fb7571a4140abc7aa1da",
+    "url": "/ohara-ui/index.html"
+  },
+  {
+    "revision": "4c9a3eb1358ab730a42e",
+    "url": "/ohara-ui/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "6358d236176dd5c6e552",
+    "url": "/ohara-ui/static/css/main.8f7071e8.chunk.css"
+  },
+  {
+    "revision": "4c9a3eb1358ab730a42e",
+    "url": "/ohara-ui/static/js/2.3afde552.chunk.js"
+  },
+  {
+    "revision": "993afbbdc88b840d216dca7eed601987",
+    "url": "/ohara-ui/static/js/2.3afde552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6358d236176dd5c6e552",
+    "url": "/ohara-ui/static/js/main.3df59267.chunk.js"
+  },
+  {
+    "revision": "d8447e5b7bdb5a0bdb8f",
+    "url": "/ohara-ui/static/js/runtime-main.555ea948.js"
+  },
+  {
+    "revision": "d0e3870ee69c32be85a04d5c138ee188",
+    "url": "/ohara-ui/static/media/Ellipsis.d0e3870e.svg"
+  }
+]);
